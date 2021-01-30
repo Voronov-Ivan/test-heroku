768fb88d9b8c05100bbf2f14aa3806aba9cbcd13
Voronov-Ivan <78203791+Voronov-Ivan@users.noreply.github.com>
2021-01-31 00:04:52 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/home.html'));
+});
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname + '/about.html'));
+});
+
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
