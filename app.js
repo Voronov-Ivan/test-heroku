86139592554ac915a7bb7f3e1fe5b129c97a8da7
Ivan Voronov <vanya.voronov.25062002@gmail.com>
2021-01-31 15:18:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;                                               //
+const port = process.env.PORT || 3000;
+
+// const events = require('events');                                               //
+// const myEmit = new events.EventEmitter();
+//
+// myEmit.on('Click', function () {
+//   res.sendFile(path.join(__dirname + '/home.html'));
+// });
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/home.html'));
