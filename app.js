7d2c8590f60b9a8ffe9b5f22eea20e18f15ddd05
Ivan Voronov <vanya.voronov.25062002@gmail.com>
2021-01-31 14:59:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
-const http = require('http');
-const server = http.createServer(function (req, res) {//
-  res.writeHead(200, {'Contebt-Type': 'text/plain'}); //
-  res.end('Hello world!');                            //
-});                                                   //
+const port = process.env.PORT || 3000;                                               //
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/home.html'));
@@ -15,4 +10,4 @@ app.get('/about', function(req, res) {
     res.sendFile(path.join(__dirname + '/about.html'));
 });
 
-server.listen(port, () => console.log(`url-shortener listening on port ${port}!`));//-app;+server
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));//-app;+server
