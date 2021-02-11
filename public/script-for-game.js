b3729b3af12023ad550e4f0075f77b4df9408503
Ivan Voronov <vanya.voronov.25062002@gmail.com>
2021-02-11 11:02:43 +0200

    54

--- public/script-for-game.js
+++ public/script-for-game.js
@@ -4,17 +4,20 @@ socket.on('players_in_room',function (players,pass) {
   $('#players').text(players);
   $('#pass').text(pass)
 });
-
 socket.on('send_kik_door',function (kik) {
-  $('#fight img').remove();
+  $('#fight #door img').remove();
   kik.forEach((item, i) => {
-    var k = "<img src='"+"public/brown/"+item+"'>"
-    $('#fight').append(k)
+    var k = "<div class='holder'><img src='"+"public/brown/"+item+"'><div class='block'><button onclick=DeleteDoorFigth("+"'"+item+"'"+")>Отбой</button><br><button onclick=TakeDoorFigth("+"'"+item+"'"+")>В руку!</button></div></div>"
+    $('#fight #door').append(k)
   });
-
-
 });
-
+socket.on('send_kik_gold',function (kik) {
+  $('#fight #gold img').remove();
+  kik.forEach((item, i) => {
+    var k = "<div class='holder'><img src='"+"public/gold/"+item+"'><div class='block'><button onclick=DeleteGoldFigth("+"'"+item+"'"+")>Отбой</button><br><button onclick=TakeGoldFigth("+"'"+item+"'"+")>В руку!</button></div></div>"
+    $('#fight #gold').append(k)
+  });
+});
 socket.on('set_kub',function (num) {
     $('#kub').text(num);
 })
@@ -31,7 +34,7 @@ socket.on('set_info', function (pl) {
         $('#PLAYER_BOX img').remove();
         item.cardInHandDoor.forEach((item, i) => {
           var divID = '#PLAYER_BOX ';
-          var img = "<img onclick=CardInFront($(this).attr('src')) src='public/brown/"+item+"'>"
+          var img = "<div class='holder'><img onclick=CardInFront($(this).attr('src')) src='public/brown/"+item+"'><div class='block'><button onclick=DoorToFigth("+"'"+item+"'"+")>На стол!</button>></div></div>"
           $(divID).append(img);
         });
       }
@@ -40,7 +43,7 @@ socket.on('set_info', function (pl) {
         $('#PLAYER_BOX2 img').remove();
         item.cardInHandGold.forEach((item, i) => {
           var divID = '#PLAYER_BOX2';
-          var img = "<img onclick=CardInFront($(this).attr('src')) src='public/gold/"+item+"'>"
+          var img = "<div class='holder'><img onclick=CardInFront($(this).attr('src')) src='public/gold/"+item+"'><div class='block'><button onclick=GoldToFigth("+"'"+item+"'"+")>На стол!</button>></div></div>"
           $(divID).append(img);
         });
       }
@@ -48,7 +51,7 @@ socket.on('set_info', function (pl) {
           $('#CardInFront img').remove();
           item.cardInFront.forEach((item, i) => {
             var divID = '#CardInFront';
-            var img = "<img onclick=CardInFront($(this).attr('src')) src='"+item+"'>"
+            var img = "<img onclick=DeleteCardInFront($(this).attr('src')) src='"+item+"'>"
             $(divID).append(img);
         });
       }
@@ -66,7 +69,7 @@ socket.on('set_info', function (pl) {
         item.cardInFront.forEach((item, a) => {
           var divID = "#card"+index;
           console.log(item);
-          var img = "<img onclick=CardInFront($(this).attr('src')) src='"+item+"'>"
+          var img = "<img src='"+item+"'>"
           $(divID).append(img);
           console.log(img);
         });
@@ -80,43 +83,63 @@ function Send_nick(nick) {
   socket.emit('Send_info',nick);
   console.log(nick);
 }
-
 function StartGame() {
   socket.emit('start_game')
 }
-
 function GetDoor() {
   socket.emit('get_door')
 }
-
 function GetGold() {
   socket.emit('get_gold')
 }
-
 function UpLVL() {
   socket.emit('up_lvl')
 }
-
 function UpDamage() {
   socket.emit('up_damage')
 }
-
 function DownLVL() {
 socket.emit('down_lvl')
 }
-
 function DownDamage() {
 socket.emit('down_damage')
 }
-
 function CardInFront(src_) {
   socket.emit('card_in_front',src_);
   }
-
 function Kub() {
   socket.emit('kub');
 }
-
 function KikDoor() {
   socket.emit('kik_door')
+}
+function KikGold() {
+  socket.emit('kik_gold')
+}
+function DeleteGoldFigth(src) {
+  console.log('1');
+  socket.emit('del_gold_figth',src);
+  console.log('2');
+}
+function DeleteDoorFigth(src) {
+  socket.emit('del_door_figth',src);
+}
+function TakeDoorFigth(src) {
+  socket.emit('take_door_figth',src)
+  DeleteDoorFigth(src)
+}
+function TakeGoldFigth(src) {
+  socket.emit('take_gold_figth',src)
+  DeleteGoldFigth(src)
+}
+function DoorToFigth(src) {
+  socket.emit('door_to_figth',src);
+}
+function GoldToFigth(src) {
+  socket.emit('gold_to_figth',src);
+}
+function DeleteCardInFront(src) {
+  console.log('1');
+  socket.emit('delete_card_in_front',src)
+  console.log('2');
 }
